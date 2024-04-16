4a0fbe790c648c3db16f1935d4df7fb092795572
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-16 19:25:51 +0400

    Add - Header component

---
+++ player/src/components/Header.svelte
@@ -0,0 +1,10 @@
+<div class="flex justify-between items-center bg-gray-100 dark:bg-gray-700 px-4 py-2">
+	<ul class="flex space-x-4">
+		<li>
+			<a href="/" class="text-gray-900 dark:text-gray-100">Home</a>
+		</li>
+		<li>
+			<a href="/convert" class="text-gray-900 dark:text-gray-100">Convert</a>
+		</li>
+	</ul>
+</div>
--- player/src/routes/+layout.svelte
+++ player/src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
 	import '../app.css';
+	import Header from '../components/Header.svelte';
 </script>
 
+<Header />
 <div class="w-full h-screen bg-cyan-500 flex items-center justify-center">
 	<div class="w-96 h-auto bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden">
 		<slot />
