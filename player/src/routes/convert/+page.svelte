cb4109b96082730b5c7e3ef446b231bf0a06789d
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-16 19:31:40 +0400

    Fix - Updated convert page

--- player/src/routes/convert/+page.svelte
+++ player/src/routes/convert/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 </script>
 
-<form method="POST">
+<h2 class="text-white">Convert Youtube Video to Audio</h2>
+<form method="POST" class="flex flex-col gap-4 text-center">
 	<input type="text" name="video_url" />
 	<button type="submit">Convert</button>
 </form>
