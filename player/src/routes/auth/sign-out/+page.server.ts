cf11a9b090072407381ad101fda5ac098d1567c1
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-20 00:17:15 +0400

    Add - Redirect when signing out

--- player/src/routes/auth/sign-out/+page.server.ts
+++ player/src/routes/auth/sign-out/+page.server.ts
@@ -1,6 +1,11 @@
-export const load: PageLoad = async ({ locals: { supabase } }) => {
+import { redirect } from "@sveltejs/kit"
+import type { PageServerLoad } from "../$types"
+
+export const load: PageServerLoad = async ({ locals: { supabase } }: any) => {
     const { error } = await supabase.auth.signOut()
     if (error) {
         console.log(error)
     }
+
+    redirect(303, "/")
 }
