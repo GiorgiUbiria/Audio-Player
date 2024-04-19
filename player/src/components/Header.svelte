d94442c617c803daaf765efd3a9c5bc4e784552d
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-20 00:01:57 +0400

    Fix - Authorization

--- player/src/components/Header.svelte
+++ player/src/components/Header.svelte
@@ -1,5 +1,9 @@
 <script lang="ts">
 	import ThemeToggle from './ThemeToggle.svelte';
+	export let data: any;
+	let { session } = data;
+	$: ({ session } = data);
+	$: ({ user } = session);
 </script>
 
 <div class="flex justify-between items-center bg-gray-100 dark:bg-gray-700 px-4 py-2">
@@ -10,6 +14,15 @@
 		<li>
 			<a href="/convert" class="text-gray-900 dark:text-gray-100">Convert</a>
 		</li>
+		{#if user}
+			<li>
+				<a href="auth/sign-out" class="text-gray-900 dark:text-gray-100"> Sign Out </a>
+			</li>
+		{:else}
+			<li>
+				<a href="auth/sign-in" class="text-gray-900 dark:text-gray-100"> Sign In </a>
+			</li>
+		{/if}
 	</ul>
 	<ThemeToggle />
 </div>
--- player/src/routes/+layout.svelte
+++ player/src/routes/+layout.svelte
@@ -3,9 +3,11 @@
 	import Header from '../components/Header.svelte';
 	import { ModeWatcher } from 'mode-watcher';
 	import { Toaster } from '$lib/components/ui/sonner';
+
+	export let data;
 </script>
 
 <ModeWatcher />
-<Header />
+<Header {data} />
 <Toaster />
 <slot />
