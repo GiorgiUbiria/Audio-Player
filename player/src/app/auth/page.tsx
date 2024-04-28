c9eb366d90827a6cfbc3751eabb58e407970d198
GiorgiUbiria <ubiriagiorgi8@gmail.com>
2024-04-28 21:14:03 +0400

    Fix - Redirect logic on auth page

--- player/src/app/auth/page.tsx
+++ player/src/app/auth/page.tsx
@@ -13,7 +13,7 @@ export default async function Page() {
   const supabase = createClient();
 
   const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
+  if (data.user) {
     redirect("/");
   }
 
